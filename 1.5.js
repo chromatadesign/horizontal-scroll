29b7e274f623ac5ebc965c05a2f2e0014968dc3f
Chromata <113706042+chromatadesign@users.noreply.github.com>
2023-06-22 13:46:00 -0400

    Update and rename ctest2.js to 1.5.js

--- 1.5.js
+++ 1.5.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-  var slider_wrapper = new Swiper(".slider-wrapper-c", {
-    wrapperClass: "slider-list-c",
-    slideClass: "slider-item-c",
+  var slider_wrapper = new Swiper(".slider-wrapper-1.5", {
+    wrapperClass: "slider-list-1.5",
+    slideClass: "slider-item-1.5",
     navigation: {
-      nextEl: '.next-slide-c',
-      prevEl: '.prev-slide-c'
+      nextEl: '.next-slide-1.5',
+      prevEl: '.prev-slide-1.5'
     },
     pagination: {
       type: 'bullets',
@@ -12,19 +12,19 @@ $(document).ready(function () {
       clickable: true,
     },
     autoplay: {
-      delay: 10,
+      delay: 1500,
       disableOnInteraction: true, 
     },
-    speed: 1500,
+    speed: 700,
     slidesPerView: 'auto',
     loop: true,
     on: {
       init: function () {
-        $('.slider-wrapper-c').on('mouseover', function () {
+        $('.slider-wrapper-1.5').on('mouseover', function () {
           slider_wrapper.autoplay.stop();
         });
 
-        $('.slider-wrapper-c').on('mouseout', function () {
+        $('.slider-wrapper-1.5').on('mouseout', function () {
           slider_wrapper.autoplay.start();
         });
       },
