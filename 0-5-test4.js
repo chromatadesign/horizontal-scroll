060f5f04a2ee73e1d211f72e539981d53c9375bf
Chromata <113706042+chromatadesign@users.noreply.github.com>
2023-07-05 09:32:11 -0400

    Update and rename 0-5-test3.js to 0-5-test4.js

--- 0-5-test4.js
+++ 0-5-test4.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
       delay: 0.1,
       disableOnInteraction: true, 
     },
-    speed: 400,
+    speed: 1000,
     slidesPerView: 'auto',
     loop: true,
     on: {
